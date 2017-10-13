8821d45e05762e483e89caba664a1d60209e5560
riyafa <riyafa.12@cse.mrt.ac.lk>
2017-10-13 15:31:44 +0530

    The for loop

--- src/test/java/org/apache/synapse/integration/EmulatorBackendTest.java
+++ src/test/java/org/apache/synapse/integration/EmulatorBackendTest.java
@@ -1,5 +1,7 @@
 package org.apache.synapse.integration;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,31 +9,53 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 
 public class EmulatorBackendTest extends BaseTest {
     String trpUrl = "http://" + getSynapseAddress() + "/services/emulator_backend";
+    private static final Log log = LogFactory.getLog(EmulatorBackendTest.class);
+    long failed = 0;
+    long passed = 0;
+    long count = 100;
 
     @Test
-    public void testEmulatorBackend() throws IOException {
+    public void testEmulatorBackend() throws MalformedURLException {
         URL url = new URL(trpUrl);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(
-                        connection.getInputStream()));
-        String inputLine;
-        inputLine = in.readLine();
-        if (inputLine == null) {
-            Assert.fail();
-        }
+        for (int i = 0; i < count; i++) {
+            try {
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                connection.getInputStream()));
+                String inputLine;
+                inputLine = in.readLine();
+                if (inputLine == null) {
+                    log.error("[" + i + "] Test failed ");
+                    failed++;
+//            Assert.fail();
+                }
 
-        while (inputLine != null) {
-            Assert.assertEquals(inputLine, "User1");
-            inputLine = in.readLine();
+                while (inputLine != null) {
+                    if (inputLine.equals("User1")) {
+                        passed++;
+                        log.info("[" + i + "] Test passed ");
+                    }
+//            Assert.assertEquals(inputLine, "User1");
+                    inputLine = in.readLine();
+                }
+                in.close();
+            } catch (Exception e) {
+                log.error("Failed ", e);
+                failed++;
+            }
         }
-        in.close();
+        log.info(count + " tests were run");
+        log.info(failed + " tests failed");
+        log.info(passed + " tests passed");
+        Assert.assertEquals(passed, count);
     }
 
 }
