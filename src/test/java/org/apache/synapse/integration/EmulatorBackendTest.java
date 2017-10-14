e888bf06dbff4c1fadb64a4f39c87749ac81d197
riyafa <riyafa.12@cse.mrt.ac.lk>
2017-10-14 19:49:03 +0530

    Use the emulator client

--- src/test/java/org/apache/synapse/integration/EmulatorBackendTest.java
+++ src/test/java/org/apache/synapse/integration/EmulatorBackendTest.java
@@ -1,54 +1,59 @@
 package org.apache.synapse.integration;
 
+import io.netty.handler.codec.http.HttpMethod;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Assert;
 import org.junit.Test;
+import org.wso2.carbon.protocol.emulator.dsl.Emulator;
+import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientConfigBuilderContext;
+import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientRequestBuilderContext;
+import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientResponseBuilderContext;
+import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientResponseProcessorContext;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URL;
 
 
 public class EmulatorBackendTest extends BaseTest {
-    String trpUrl = "http://" + getSynapseAddress() + "/services/emulator_backend";
     private static final Log log = LogFactory.getLog(EmulatorBackendTest.class);
-    long failed = 0;
-    long passed = 0;
-    long count = 100;
+    private long failed = 0;
+    private long passed = 0;
 
     @Test
     public void testEmulatorBackend() throws MalformedURLException {
-        URL url = new URL(trpUrl);
-        for (int i = 0; i < count; i++) {
-            try {
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("POST");
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(
-                                connection.getInputStream()));
-                String inputLine;
-                inputLine = in.readLine();
-                if (inputLine == null) {
-                    log.error("[" + i + "] Test failed ");
-                    failed++;
-//            Assert.fail();
-                }
 
-                while (inputLine != null) {
-                    if (inputLine.equals("User1")) {
-                        passed++;
-                        log.info("[" + i + "] Test passed ");
-                    }
-//            Assert.assertEquals(inputLine, "User1");
-                    inputLine = in.readLine();
+        long count = 1000;
+        for (int i = 1; i <= count; i++) {
+            try {
+                HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                        .client()
+                        .given(HttpClientConfigBuilderContext.configure()
+                                       .host(getConfig().getSynapseServer().getHostname())
+                                       .port(Integer.parseInt(getConfig().getSynapseServer().getPort())))
+                        .when(HttpClientRequestBuilderContext.request()
+                                      .withPath(
+                                              "/services/emulator_backend")
+                                      .withMethod(
+                                              HttpMethod
+                                                      .POST))
+                        .then(HttpClientResponseBuilderContext.response()
+                                      .assertionIgnore())
+                        .operation()
+                        .send();
+                if (response == null) {
+                    log.error("[" + i + "] Failed: response is null");
+                    failed++;
+                    continue;
                 }
-                in.close();
+                if (response.getReceivedResponseContext().getResponseBody().equals("User1")) {
+                    log.info("[" + i + "] Passed");
+                    passed++;
+                } else {
+                    log.error("[" + i + "] Failed: response is incorrect");
+                    failed++;
+                }
             } catch (Exception e) {
-                log.error("Failed ", e);
+                log.error("[" + i + "] Failed", e);
                 failed++;
             }
         }
